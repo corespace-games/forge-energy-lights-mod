f7d74fafa1568bc2482e1dbdb025bb0f26b4ca6f
AsP3X <development@vorberg-webmail.de>
2023-04-13 10:43:20 +0200

    REFACTOR: changed tab grouping to the felights custom tab

--- src/main/java/de/corespace/felights/item/ModItems.java
+++ src/main/java/de/corespace/felights/item/ModItems.java
@@ -1,7 +1,6 @@
 package de.corespace.felights.item;
 
 import de.corespace.felights.FELights;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,10 +12,10 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FELights.MOD_ID);
 
     public static final RegistryObject<Item> TUNGSTEN_INGOT = ITEMS.register("tungsten_ingot",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FELIGHTS_TAB)));
 
     public static final RegistryObject<Item> RAW_TUNGSTEN = ITEMS.register("raw_tungsten",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FELIGHTS_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
